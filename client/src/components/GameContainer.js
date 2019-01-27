5f0625743a3060ac62ada2f35f00c12a85fbff69
tammam1998 <tammammusatafa@gmail.com>
2019-01-26 21:19:49 -0500

    https

--- client/src/components/GameContainer.js
+++ client/src/components/GameContainer.js
@@ -21,7 +21,7 @@ export default class GameContainer extends React.Component {
     constructor(props) {
         super(props);
 
-        this.socket = io("http://newsracer.herokuapp.com");
+        this.socket = io("https://newsracer.herokuapp.com");
 
         this.socket.on("start_game", () =>{
             console.log("client recieved news");
