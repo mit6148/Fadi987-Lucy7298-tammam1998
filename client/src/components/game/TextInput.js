f82eb48d8b150b8bfad1f4ff428e053159052811
tammam1998 <tammammusatafa@gmail.com>
2019-01-28 13:31:51 -0500

    added auto focus

--- client/src/components/game/TextInput.js
+++ client/src/components/game/TextInput.js
@@ -16,6 +16,7 @@ export default class TextInput extends React.Component {
             indexOfError : -1,
         };
         this.updateInputValue = this.updateInputValue.bind(this);
+        this.textInput = React.createRef();
     }
 
     updateInputValue(event) {
@@ -52,12 +53,22 @@ export default class TextInput extends React.Component {
         this.props.updateTypedWord(inpVal)
     }
 
+    focusTextInput() {
+        // Explicitly focus the text input using the raw DOM API
+        // Note: we're accessing "current" to get the DOM node
+        this.textInput.current.focus();
+      }
+
+    componentDidUpdate() {
+        this.focusTextInput();
+    }
+
 
     render() {
         return (
             <div className="input-group mb-3">
                 <input type="text" className="form-control" style = {this.state.color} placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"
-                    value={this.state.inputValue} onChange={event => this.updateInputValue(event)} />
+                    value={this.state.inputValue} onChange={event => this.updateInputValue(event) } autoFocus disabled={this.props.disabled} ref = {this.textInput}/>
                 <div className="input-group-append">
                     <button type="button" className="btn btn-dark" onClick={this.props.handleInput}>NextWord</button>
                 </div>
